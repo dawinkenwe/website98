0a2c3096bc6fd170ddc53939425122b0be584125
dawinkenwe <dwinkenwerder@gmail.com>
2024-06-21 13:04:49 -0700

    Start button fixed. Need to fix start menu. might move into taskbar.

--- src/components/StartButton.js
+++ src/components/StartButton.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import startbuttonimage from '../img/win98.gif';
+import startbuttonimage from '../img/win98start.png';
+import './StartButton.css'
 
 const StartButton = ({ onClick }) => (
-    <div id="start-button" class="windows-box-shadow" onClick={onClick}><img src={startbuttonimage}></img></div>
+    <div id="start-button" class="windows-box-shadow" onClick={onClick} style={{ backgroundImage: `url(${startbuttonimage})` }}></div>
 );
 
 /*<button id="start-button" class="windows-box-shadow" onClick={onClick} style={{ backgroundImage: startbuttonimage}}></button> */
