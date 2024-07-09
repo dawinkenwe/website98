d4dcc819ae4c22e375cb8544fced1d416231acf2
dawinkenwe <dwinkenwerder@gmail.com>
2024-07-09 15:59:13 -0700

    setup github pages and github actions for deployment

--- src/data/PatchNotesData.js
+++ src/data/PatchNotesData.js
@@ -4,6 +4,7 @@ const patchNotesData = {
         description: "This is the initial release! It was finally launched!",
         featureList: [
             "A start menu with five functional apps: help, windows media player, windows update, Documents, and Notepad",
+            "Notepad opens the most recent blog / text post. Documents displays a list of all blogs",
             "Windows that can be resized and closed",
             "Windows stack on top of each other based off of most recently accessed.",
             "Windows can be minimized by clicking the minimize button or the taskbar",
