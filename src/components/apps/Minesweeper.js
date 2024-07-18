bf8771b35c3f0c0db465be10814d9cdb24cf666a
dawinkenwe <dwinkenwerder@gmail.com>
2024-07-17 22:26:14 -0700

    Minesweeper shows grid at game over. still needs styling and to cascade on empty square selection. reuse mine placing neighbor logic? make set?

--- src/components/apps/Minesweeper.js
+++ src/components/apps/Minesweeper.js
@@ -5,15 +5,13 @@ import {produce} from 'immer';
 const adjacentIndexOffsets = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]];
 
 const createNewGrid = (numRows, numCols, numMines) => {
+	console.log('CREATING NEW GRID');
 	const grid = Array.from({ length: numRows }, () => {
 		return (Array.from({ length: numCols }, () => ({ hasMine: false, adjacencyCount: 0, display: '' })))
 	});
 
-	console.log(grid);
-
 	const minePositions = new Set();
 
-	// while set length < numMines
 	while (minePositions.size < numMines) {
 		const newX = Math.floor(Math.random() * 9);
 		const newY = Math.floor(Math.random() * 9);
@@ -27,19 +25,17 @@ const createNewGrid = (numRows, numCols, numMines) => {
 			let dx = offset[0];
 			let dy = offset[1];
 			if (-1 < x + dx && x + dx < numRows && -1 < y + dy && y + dy < numCols) {
-				console.log(grid);
 				grid[x + dx][y + dy].adjacencyCount += 1;
 			}
 		}
 	}
-	console.log(grid);
 	return grid;
 };
 
 // TODO: Add css, make it so end actually shows all squares, etc.
 
 const MineSweeper = () => {
-	const [grid, setGrid] = useState(createNewGrid(9, 9, 9));
+	const [grid, setGrid] = useState(() => createNewGrid(9, 9, 9));
 	const [gameOver, setGameOver] = useState(false);
 	const rows = 9;
 	const columns = 9;
@@ -48,26 +44,34 @@ const MineSweeper = () => {
 		console.log('GAME OVER');
 		setGameOver(true);
 		setGrid(produce(grid, draft => {
+			console.log('updating state for end game');
 			draft.forEach(row => {
 				row.forEach(cell => {
 					if (cell.hasMine) {
-						cell.value = 'm';
+						console.log('updating m');
+						cell.display = 'm';
 					} else if (cell.adjacencyCount) {
-						cell.value = cell.adjacencyCount;
+						console.log('updating num');
+						cell.display = cell.adjacencyCount;
+					} else {
+						console.log('updating _');
+						cell.display = '_';
 					}
-				})
-			})
-		}))
+				});
+			});
+		}));
+		console.log(grid);
 	};
+
+	console.log("Component re-rendering with grid:", grid);
 
 	const handleLeftClick = (x, y) => {
 		// Note - react state checks for re-renders as a NEW OBJECT.
 		// We need to return a NEW OBJECT if we want to update the state.
-		console.log('handling left click');
-		console.log('x: ' + x + ' y: ' + y);
 		if (gameOver) {
 			return;
 		}
+		console.log('clicked on x: ' + x + ' y: ' + y);
 		if (grid[x][y].display && grid[x][y].display !== 'f') {
 			return;
 		}
@@ -78,6 +82,7 @@ const MineSweeper = () => {
 				const cell = draft[x][y];
 				cell.display = cell.adjacencyCount ? cell.adjacencyCount : '_';
 			}));
+			console.log(grid);
 		}
 	}
 
