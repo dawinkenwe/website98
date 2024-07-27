88ec88abe8ac0a1a2aa93896604f5fb97fbc7834
dawinkenwe <dwinkenwerder@gmail.com>
2024-07-26 17:52:01 -0700

    Clean up 7 segment display and make sections scalable and on or off based off of number

--- src/components/apps/MineSweeper/SevenSegmentDisplay.js
+++ src/components/apps/MineSweeper/SevenSegmentDisplay.js
@@ -1,17 +1,47 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 
-const SevenSegmentDisplay = () => {
+const litSegments = {
+    0: [0, 1, 2, 4, 5, 6],
+    1: [2, 4],
+    2: [0, 2, 3, 4, 6],
+    3: [0, 2, 3, 5, 6],
+    4: [1, 2, 3, 5],
+    5: [0, 1, 3, 5, 6],
+    6: [0, 1, 3, 4, 5, 6],
+    7: [0, 2, 5],
+    8: [0, 1, 2, 3, 4, 5, 6],
+    9: [0, 1, 2, 3, 5, 6]
+};
+
+const generateSegmentPaths = (scale) => {
+    const basePaths = [
+        'M 2 2 L 3 1 L 7 1 L 8 2 L 7 3 L 3 3 Z',
+        'M 2 2 L 3 3 L 3 7 L 2 8 L 1 7 L 1 3 Z',
+        'M 7 3 L 8 2 L 9 3 L 9 10 L 8 11 L 7 10 Z',
+        'M 2 8 L 3 7 L 7 7 L 8 8 L 7 9 L 3 9 Z',
+        'M 1 9 L 2 8 L 3 9 L 3 13 L 2 14 L 1 13 Z',
+        'M 7 9 L 8 8 L 9 9 L 9 13 L 8 14 L 7 13 Z',
+        'M 2 14 L 3 13 L 7 13 L 8 14 L 7 15 L 3 15 Z'
+    ];
+
+    const scaledPaths = basePaths.map(path => path.replace(/(\d+)/g, match => match * scale))
+    return scaledPaths;
+};
+
+
+
+const SevenSegmentDisplay = ({ scale = 2, value = 0 }) => {
+    const segmentPaths = useMemo(() => generateSegmentPaths(scale), [scale])
     return (
         <svg height="48 width=30" xmlns="segmentdisplay">
-            <path id="segment1" d="M 6 6 L 9 3 L 21 3 L 24 6 L 21 9 L 9 9 Z" stroke="black" fill="red" stroke-width="1"></path>
-            <path id="segment2" d="M 3 9 L 6 6 L 9 9 L 9 21 L 6 24 L 3 21 Z" stroke="black" fill="red" stroke-width="1"></path>
-            <path id="segment3" d="M 21 9 L 24 6 L 27 9 L 27 21 L 24 24 L 21 21 Z" stroke="black" fill="red" stroke-width="1"></path>
-            <path id="segment4" d="M 6 24 L 9 21 L 21 21 L 24 24 L 21 27 L 9 27 Z" stroke="black" fill="red" stroke-width="1"></path>
-            <path id="segment5" d="M 6 24 L 9 27 L 9 39 L 6 42 L 3 39 L 3 27 Z" stroke="black" fill="red" stroke-width="1"></path>
-            <path id="segment5" d="M 24 24 L 27 27 L 27 39 L 24 42 L 21 39 L 21 27 Z" stroke="black" fill="red" stroke-width="1"></path>
-            <path id="segment6" d="M 6 42 L 9 39 L 21 39 L 24 42 L 21 45 L 9 45 Z" stroke="black" fill="red" stroke-width="1"></path>
+            {segmentPaths.map((path, index) => (
+                <path
+                    id={`segment${index}`}
+                    d={path}
+                    fill={litSegments[value].includes(index) ? 'red' : 'lightgray'} />
+            )) }
         </svg>
-    );
-}
+    )
+};
 
 export default SevenSegmentDisplay;
