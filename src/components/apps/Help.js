4e19b67c77c2da0f5b083bab94f134bc08d2cab7
dawinkenwe <dwinkenwerder@gmail.com>
2026-07-24 12:51:18 -0700

    first checking for new version of help

--- src/components/apps/Help.js
+++ src/components/apps/Help.js
@@ -5,11 +5,11 @@ import './Help.css';
 import DOMPurify from 'dompurify';
 
 const Help = () => {
-    const [selectedQuestion, setSelectedQuestion] = useState(Object.keys(helpData)[0]);
+    const [selectedTab, setSelectedTab] = useState("Contents");
     const img = getProgramIcon('help');
 
     const handleQuestionClick = (question) => {
-        setSelectedQuestion(question);
+        setSelectedTab(question);
     };
 
     const createMarkup = (text) => {
@@ -17,31 +17,36 @@ const Help = () => {
         return { __html: DOMPurify.sanitize(textBR) };
     }
 
+    const getTabContents = (title) => {
+        if (title === "Help") {
+            return(
+                <>
+                    <h2>About Me</h2>
+                    <p>My name is David Winkenwerder. I am a software engineer and nerd based in Sunnyvale, California. I have over 8 years of experience in professional software development. I enjoy learning new languages and have worked in C, Python, Ruby, and Javascript. I have dabbled in Golang, Java, Lua, and even a little Scala in college.</p><p>When I am not working, I am usually playing games, reading, or spending time with my cats. I especially enjoy social games that can bring friends together, such as Hues and Clues, Codenams, and Crokinole. Or any of the wonderful new friend slop games of the week such as Meccha Chameleon or Super Battle Golf.</p>
+                </>
+            )
+        }
+    }
+
     return (
-        <div id="help">
-            <div className="top-bar">
-                <div>Location</div>
-                <input type="text" id="long" className="inverse-windows-box-shadow" value="General Help" readOnly=''></input>
-            </div>
-            <div className="help-contents">
-                <div className="tab-box">
-                    <div className="tab-header">Help</div>
-                    <ul className="help-questions">
-                        {Object.keys(helpData).map((question) => (
-                            <li key={question} onClick={() => handleQuestionClick(question)} className={`${question === selectedQuestion ? 'selected-question' : ''}` }>
-                                <img src={img} alt="help" className="help-img"/>
-                                {question}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="answers">
-                    {selectedQuestion &&
-                        (<div className="content" dangerouslySetInnerHTML={createMarkup(helpData[selectedQuestion])} />
-                    )}
+        <>
+        <div>
+            <menu role="tablist">
+                <li role="tab" aria-selected={selectedTab === "Contents"} onClick={() => setSelectedTab("Contents")}><a href="#tabs">Contents</a></li>
+                <li role="tab" aria-selected={selectedTab === "Index"} onClick={() => setSelectedTab("Index")}><a href="#tabs">Index</a></li>
+                <li role="tab" aria-selected={selectedTab === "Search"} onClick={() => setSelectedTab("Search")}><a href="#tabs">Search</a></li>
+            </menu>
+            <div class="window" role="tabpanel" style={{resize: 'none'}}>
+                <div class="window-body">
+                    <p>the tab content</p>
                 </div>
             </div>
         </div>
+        <div>
+            <p>hello</p>
+            <p>world</p>
+        </div>
+    </>
     );
 };
 
