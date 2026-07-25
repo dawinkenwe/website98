4c2d39b6728c666281710e85ab6cc2019ab1b01b
dawinkenwe <dwinkenwerder@gmail.com>
2026-07-24 17:06:53 -0700

    Add content to a few of the Help menu options

--- src/components/apps/Help.js
+++ src/components/apps/Help.js
@@ -26,6 +26,35 @@ const Help = () => {
                     <p>My name is David Winkenwerder. I am a software engineer and nerd based in Sunnyvale, California. I have over 8 years of experience in professional software development. I enjoy learning new languages and have worked in C, Python, Ruby, and Javascript. I have dabbled in Golang, Java, Lua, and even a little Scala in college.</p><p>When I am not working, I am usually playing games, reading, or spending time with my cats. I especially enjoy social games that can bring friends together, such as Hues and Clues, Codenams, and Crokinole. Or any of the wonderful new friend slop games of the week such as Meccha Chameleon or Super Battle Golf.</p>
                 </>
             )
+        } else if (title === "Resume") {
+            return(
+                <>
+                    <h2>Resume</h2>
+                    <h3>Skills and Technologies</h3>
+                    <p></p>
+                    <h3>Experience</h3>
+                    <p></p>
+                    <h3>Education</h3>
+                    <p>UC Santa Barbara - BS in Computer Science</p>
+                </>
+            )
+        } else if (title === "WhatFor") {
+            return (
+                <>
+                    <h2>What is this for?</h2>
+                    <p>Fun!</p>
+                    <p>And learning of course. And a fun way to display relevant information for recruiters and potential companies.</p>
+                    <p>But mostly fun.</p>
+                </>
+            )
+        } else if (title === "Why"){
+            return(
+                <>
+                    <h2>Why Windows98?</h2>
+                    <p>I wanted to create a website that reminded me of some of my earliest memories with computers.I have very fond memories of playing Oregon Trail and Mathblaster in the computer lab at my elementary school. And of drawing in MS paint when I was spending time with my Grandfather at his office.</p>
+                    <p>I was also heavily inspired by <a href = 'https://dev.to/smpnjn/using-only-css-to-recreate-windows-98-47b1' target='_blank' >this article</a > which made a windows98 like interface with pure CSS, and wanted to use <a href = 'https://jdan.github.io/98.css/' target='_blank'> this project</a > which provides a css library for windows98 like styling.</p>
+                </>
+            )
         }
     }
 
@@ -34,10 +63,11 @@ const Help = () => {
         if (title === "Contents") {
             return(
                 <menu className="help-questions">
-                    <li className={`${false ? 'selected-question' : 'unselected-question'}`} onClick={() => {setSelectedHelp("Welcome")}}><img src={getProgramIcon('help')} alt="help" className="help-img" />Welcome</li>
-                    <li><img src={getProgramIcon('help')} alt="help" className="help-img" />Resume</li>
-                    <li><img src={getProgramIcon('help')} alt="help" className="help-img" />What is this for?</li>
-                    <li><img src={getProgramIcon('help')} alt="help" className="help-img" />What can I do here?</li>
+                    <li className={`${selectedHelp === "Welcome" ? 'selected-question' : 'unselected-question'}`} onClick={() => {setSelectedHelp("Welcome")}}><img src={getProgramIcon('help')} alt="help" className="help-img" />Welcome</li>
+                    <li className={`${selectedHelp === "Resume" ? 'selected-question' : 'unselected-question'}`} onClick={() => {setSelectedHelp("Resume")}}><img src={getProgramIcon('help')} alt="help" className="help-img" />Resume</li>
+                    <li className={`${selectedHelp === "WhatFor" ? 'selected-question' : 'unselected-question'}`} onClick={() => {setSelectedHelp("WhatFor")}}><img src={getProgramIcon('help')} alt="help" className="help-img" />What is this for?</li>
+                    <li className={`${selectedHelp === "WhatDo" ? 'selected-question' : 'unselected-question'}`} onClick={() => {setSelectedHelp("WhatDo")}}><img src={getProgramIcon('help')} alt="help" className="help-img" />What can I do here?</li>
+                    <li className={`${selectedHelp === "Why" ? 'selected-question' : 'unselected-question'}`} onClick={() => {setSelectedHelp("Why")}}><img src={getProgramIcon('help')} alt="help" className="help-img" />Why Windows98?</li>
                 </menu>
             )
         }
