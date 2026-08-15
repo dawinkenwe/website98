1baa880f88e153701820d72702aa45b131ed361f
dawinkenwe <dwinkenwerder@gmail.com>
2026-08-14 19:05:20 -0700

    add a select for the location to the explore. override the 98.css behavior to make sure it is white background black text even whken selected.

--- src/components/apps/Explorer.js
+++ src/components/apps/Explorer.js
@@ -1,25 +1,41 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import { useAppContext } from '../../AppContext';
+import MenuBar from './MenuBar'
 import './Explorer.css'
 
 const Explorer = () => {
     const { state, dispatch } = useAppContext();
+    const [ selectedObjects, setSelectedObjects ] = useState([])
+    const [ selectedFolder, setSelectedFolder ] = useState("My Computer")
+    const options = {
+        File: ["New", "Open", "Save", "Exit"],
+        Edit: ["Undo", "Cut", "Copy", "Paste"],
+        View: ["Zoom In", "Zoom Out", "Fullscreen"],
+        Tools: ["Settings", "Options"],
+        Help: ["About", "Documentation"],
+    };
 
     return (
         <div className="explorer-body">
-            <div className="explorer-menu-bar">1</div>
+            <div className="explorer-menu-bar">
+                <MenuBar options={options}></MenuBar>
+            </div>
 
-            <div className="row-two-left">2A</div>
+            <div className="row-two-left">
+                <select>
+                    <option>My Computer</option>
+                </select>
+            </div>
             <div className="row-two-right">2B</div>
 
-            <div className="row-three-left">3A</div>
-            <div className="row-three-right">3B</div>
+            <div className="row-three-left">All Folders</div>
+            <div className="row-three-right">Contents of '{selectedFolder}'</div>
 
             <div className="row-four-left">4A</div>
             <div className="row-four-right">4B</div>
 
             <div className="bottom-bar">
-                <div>5A</div>
+                <div>{selectedObjects.length} object(s)</div>
                 <div>5B</div>
             </div>
         </div>
